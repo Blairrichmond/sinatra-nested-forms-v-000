59fc4d049a67510ec819bb75cdd73cac5806b883
Brent Ransom <sadumedusa@gmail.com>
2019-02-24 22:32:13 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+   attr_accessor :name, :type, :booty
+  
+  @@ships = []
+  
+   def initialize(args)
+    @name = args[:name]
+    @weight = args[:type]
+    @height = args[:booty]
+    ships << self 
+  end
+  
+  def self.all 
+    @@ships
+  end
+  
+  def self.clear
+    @@ships = []
+  end
 end
